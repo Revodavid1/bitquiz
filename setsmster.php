08ac19cc26ed5e50a6ef5b34308fe654d1211145
Revodavid1 <32637870+Revodavid1@users.noreply.github.com>
2017-12-09 09:09:23 -0500

    Update setsemester.php

--- setsmster.php
+++ setsmster.php
@@ -21,51 +21,49 @@ if(!isset($_SESSION["logged_in_admin_email"])){
 	<?php require "layout/adminnav.php" ?>
 	
 	<div class="jumbotron mt-1" style="background-image: url('bq main.PNG');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;opacity: 0.8;">
-  			<h1 class="display-3 text-white">BitQuiz</h1>
-  			<p class="lead text-white">#1 choice for classroom quiz.</p>
+  		<h1 class="display-3 text-white">BitQuiz</h1>
+  		<p class="lead text-white">#1 choice for classroom quiz.</p>
 	</div>
 	
 	<?php
-	$saveSemester = new Semester;
-	
-	if (isset($_POST["saveSmster"])){
-		$smter = $_POST["semester"];
-		$year = $_POST["smsYear"];
-		$fullsemester = $smter.' '.$year;
-		$saveSemester->updSmster($fullsemester);
-	} 
+		$saveSemester = new Semester;
+		
+		if (isset($_POST["saveSmster"])){
+			$smter = $_POST["semester"];
+			$year = $_POST["smsYear"];
+			$fullsemester = $smter.' '.$year;
+			$saveSemester->updSmster($fullsemester);
+		} 
 	?>
 
 	<div class="text-center">
 		<div class="border border-dark">
 			<form class="m-2" method="POST">	
-				<div class="row">
-			    <div class="col-md-12">
+			<div class="row">
+				<div class="col-md-12">
 			      <label>Current Semester: 
 			      <span> 
 			      	<?php
 			      		$chkSmster = new Semester();
 			      		$chkSmster -> chkSmster();	
 			      	?>
-			      	</span> 
-			      	</label>
+			      </span> 
+			      </label>
 			     </div>
-			     </div>
-			    <div class="row">
-			    <div class="form-inline col-md-12">
-			      <label class="col-md-4 form-group">Set Semester: </label> 
+			</div>
+			<div class="row">
+				<div class="form-inline col-md-12">
+			    	<label class="col-md-4 form-group">Set Semester: </label> 
 			      	<select class="col-md-4 form-control form-control-sm form-group" name="semester" required>
 			      		<option value="">Select Semester</option>
 			      		<option value="Fall">Fall</option>
 			      		<option value="Spring">Spring</option>
 			      		<option value="Summer">Summer</option>
 			      	</select>
-			      	
 			      	<input type="text" class="col-md-4 ml-1 text-center form-control-sm" name="smsYear" placeholder="YEAR" value="<?php echo Date('Y');?>" required/>
 			     </div>
-			     </div>
-			  
-			  <input type="submit" class="btn btn-primary mt-1" name="saveSmster" value="Save"/>
+			</div>
+				<input type="submit" class="btn btn-primary mt-1" name="saveSmster" value="Save"/>
 			</form>
 		</div>
 	</div>
