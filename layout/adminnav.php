c21cca74ecacebb350e52243ac9679f7d8260642
Revodavid1 <32637870+Revodavid1@users.noreply.github.com>
2017-12-09 08:29:25 -0500

    Create adminnav.php

---
+++ layout/adminnav.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<a class="navbar-brand" href="#">
+    <img src="img/bq main.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    BitQuiz
+  	</a>
+  
+  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  	</button>
+
+  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    	<ul class="navbar-nav mr-auto">
+      		<li class="nav-item">
+        		<a class="nav-link" href="admindashboard.php">Home <span class="sr-only">(current)</span></a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="adduser.php">Create Users</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="addcourse.php">Add Courses</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="chginstr.php">Change Quiz Instructors</a>
+      		</li>
+      		<li class="nav-item">
+        		<a class="nav-link" href="setsmster.php">Set Semester</a>
+      		</li>
+      		<form class="form-inline my-2 my-lg-0">
+		      <a href="adminlogin.php" class="btn btn-outline-success my-2 my-sm-0">Logout</a>
+		    </form>
+    	</ul>
+  </div>
+  
+</nav>
